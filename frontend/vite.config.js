e64eeb0d5c17ca6a33f7e3ffd71111facb7229ef
ziadtarek12 <zi3dtarek@gmail.com>
2025-12-20 19:05:01 +0200

    Fixing vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
+        host: true,
+        allowedHosts: true,
         proxy: {
             '/api': {
                 target: 'http://127.0.0.1:8000',
